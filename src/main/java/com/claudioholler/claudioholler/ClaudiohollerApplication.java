e8d7857a2179cefa9b9e97c07a4c0cd3eb8e8665
unknown <claudioholler@gmail.com>
2020-04-09 16:08:01 -0300

    Projeto Criado

---
+++ src/main/java/com/claudioholler/claudioholler/ClaudiohollerApplication.java
@@ -0,0 +1,13 @@
+package com.claudioholler.claudioholler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaudiohollerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaudiohollerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/claudioholler/claudioholler/ClaudiohollerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claudioholler.claudioholler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaudiohollerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
