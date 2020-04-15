0de8ebe64a0d2d3d3cc93d4763e8aa17ce9297ed
unknown <claudioholler@gmail.com>
2020-04-14 21:22:40 -0300

    Primeira Classe de dominio

--- src/main/java/com/claudioholler/claudioholler/resources/CategoriaResource.java
+++ src/main/java/com/claudioholler/claudioholler/resources/CategoriaResource.java
@@ -1,16 +1,30 @@
 package com.claudioholler.claudioholler.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.claudioholler.claudioholler.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")//ENDPOINT categorias
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET) //REST - http GET
-	public String listar(){
-		return "REST está funcionando";
+	public List<Categoria> listar(){
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		//return "REST está funcionando";
+	    return lista;
 	}
 
 }
