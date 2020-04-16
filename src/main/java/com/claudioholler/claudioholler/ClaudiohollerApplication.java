506487eb54ec9b970b0b322f8b3510e64fee630c
unknown <claudioholler@gmail.com>
2020-04-16 19:42:03 -0300

    Criando operacao de instanciacao

--- src/main/java/com/claudioholler/claudioholler/ClaudiohollerApplication.java
+++ src/main/java/com/claudioholler/claudioholler/ClaudiohollerApplication.java
@@ -1,14 +1,33 @@
 package com.claudioholler.claudioholler;
 
+import org.assertj.core.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ClaudiohollerApplication {
+import com.claudioholler.claudioholler.domain.Categoria;
+import com.claudioholler.claudioholler.respositories.CategoriaRespository;
 
+@SpringBootApplication
+public class ClaudiohollerApplication implements CommandLineRunner {
+
+	@Autowired //instanciar automaticamente no sprinboot
+	private CategoriaRespository categoriaRepository;
+	
 	public static void main(String[] args) {
 		
 		SpringApplication.run(ClaudiohollerApplication.class, args);
+	}
+
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepository.save(cat1);
+		categoriaRepository.save(cat2);
+		
 	}
 
 }
